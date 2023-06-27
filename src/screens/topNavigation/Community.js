466b52228d8573abc5fc7cb5c0754afc3ed6b77a
ankit9758 <ankitsingh9758@gmail.com>
2023-06-27 19:15:12 +0530

    top navigation

--- src/screens/topNavigation/Community.js
+++ src/screens/topNavigation/Community.js
@@ -1,13 +1,51 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { StyleSheet,View,Text } from 'react-native';
-import { white } from '../../../utils/color';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { black, white } from '../../../utils/color';
+import { GoogleSignin, statusCodes, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+
+
 
 
 
 const Community = () => {
+    const [userData, setUserData] = useState(null)
+    useEffect(() => {
+        GoogleSignin.configure({ webClientId: '300110096690-t6kf0n4efbsr5t2m4tetqpp8l5fa2n6q.apps.googleusercontent.com' });
+    })
+
+    // Somewhere in your code
+    const signIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            console.log('google data ', userInfo.user)
+            setUserData({ userInfo });
+        } catch (error) {
+            console.log('google data error', error)
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                // user cancelled the login flow
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                // operation (e.g. sign in) is in progress already
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                // play services not available or outdated
+            } else {
+                // some other error happened
+            }
+        }
+    };
     return (
         <View style={style.container}>
-            <Text>Community </Text>
+            {/* <GoogleSigninButton
+                size={GoogleSigninButton.Size.Wide}
+                color={GoogleSigninButton.Color.Dark}
+                onPress={()=>{signIn()}}
+               // disabled={this.state.isSigninInProgress}
+    /> */}
+            <TouchableOpacity onPress={()=>signIn()}>
+                <Text style={{width:100,color:black,height:50}}>
+                     Google Sign in 
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 
--- src/screens/topNavigation/PagerTabs.js
+++ src/screens/topNavigation/PagerTabs.js
@@ -1,17 +1,21 @@
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import ChangePassword from '../ChangePassword';
-import SavedAddress from '../SavedAddress';
 import CallList from './Calls';
 import Status from './Status';
 import ChatList from './ChatList';
-import { black, red, white } from '../../../utils/color';
+import { black, grey, red, white } from '../../../utils/color';
 import Community from './Community';
+import { Image, Text, View, StyleSheet } from 'react-native';
+import { image_group } from '../../../utils/images';
 const Tab = createMaterialTopTabNavigator();
 
 function MyTabs() {
     return (
         <Tab.Navigator
+            initialRouteName='ChatList'
             screenOptions={{
+                tabBarActiveTintColor: white,
+                tabBarInactiveTintColor: black,
+                tabBarAllowFontScaling: true,
                 tabBarIndicatorStyle: {
                     backgroundColor: '#00000000',
                     height: '100%',
@@ -20,29 +24,108 @@ function MyTabs() {
                 },
                 tabBarLabelStyle: {
                     fontFamily: 'Raleway-Regular',
-                    fontSize: 14,
+                    //  fontSize: 14,
                     paddingVertical: 5,
-                    color: black
+                    // color: black
                 },
                 tabBarStyle: {
                     backgroundColor: red
                 }
+                ,
+
 
 
 
             }}
         >
 
-            <Tab.Screen name="Community" component={Community} options={{ title: 'Com' }} />
+            <Tab.Screen name="Community"
+                component={Community}
+                options={{
+                    tabBarIcon: ({ focused }) => (
+                        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                            <Image source={image_group} resizeMode="contain" style={[styles.bottomTabIcon, { tintColor: focused ? white : black }]} />
 
-            <Tab.Screen name="CallList" component={CallList} options={{ title: 'Calls' }} />
+                        </View>)
+                    //,tabBarLabelStyle:{display:'none'}
+                    , tabBarShowLabel: false
+                }}
+            />
 
-            <Tab.Screen name="ChatList" component={ChatList} options={{ tabBarLabel: 'Chats' }} />
-            <Tab.Screen name="Status" component={Status} />
+
+            <Tab.Screen name="ChatList" component={ChatList} options={{
+                tabBarLabel: ({ focused }) => (
+
+                    <View style={{ flexDirection: 'row' }}>
+                        <Text style={{
+                            fontFamily: focused ? 'Raleway-Regular' : 'Raleway-Regular'
+                            , fontSize: 14, color: focused ? white : black
+                        }}>Chats</Text>
+
+                        <Text style={{
+                            fontFamily: focused ? 'Raleway-Regular' : 'Raleway-Regular'
+                            , fontSize: 10, color: focused ? red : black,
+                            backgroundColor:focused?'white':grey,
+                            width: 20, 
+                            height: 20,
+                            marginStart:7,
+                           
+                            alignSelf:'center',
+                            textAlign:'center',
+                             borderRadius: 40 / 2,
+                        }}>3</Text>
+
+                    </View>
+
+                )
+            }
+
+            } />
+            <Tab.Screen name="CallList"
+                component={CallList} options={{
+                    tabBarLabel: ({ focused }) => (
+                        <View>
+                            <Text style={{
+                                fontFamily: focused ? 'Raleway-Regular' : 'Raleway-Regular'
+                                , fontSize: 14, color: focused ? white : black
+                            }}>Calls</Text>
+                        </View>
+
+                    )
+                }}
+
+            />
+            <Tab.Screen name="Status" component={Status} options={{
+                tabBarLabel: ({ focused }) => (
+                    <Text style={{
+                        fontFamily: focused ? 'Raleway-Regular' : 'Raleway-Regular'
+                        , fontSize: 14, color: focused ? white : black
+                    }}>Status</Text>
+                ),
+
+            }} />
         </Tab.Navigator>
     );
 }
+const styles = StyleSheet.create({
 
+    bottomTabIcon: {
+        width: 20,
+        height: 20,
+        tintColor: white
+
+    },
+    appTextBold14: {
+        fontSize: 14,
+        color: white,
+        textShadowRadius: 10,
+        borderWidth: 2,
+        textShadowOffset: { width: 5, height: 5 },
+        borderColor: white
+
+
+    },
+})
 
 export default TabTop = () => {
     return <MyTabs />
